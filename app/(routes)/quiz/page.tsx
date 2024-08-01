2b87f85bfd8d385e1ddee05813fbb271a12c5522
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2024-08-01 12:02:08 +0300

    Kurulum & Clerk Entegrasyonu

---
+++ app/(routes)/quiz/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    ClerkProvider,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+  } from '@clerk/nextjs'
+
+const page = () => {
+  return (
+    <div className='text-white'>
+      quiz
+    </div>
+
+  )
+}
+
+export default page
